2f4237f1279a182155d81f06ebc1a21b211338ef
Yuuta Mikami <ougiakg@gmail.com>
2018-07-05 14:46:01 +0900

    三回目のコミット

--- Hellow World/src/sample/Hellowworld.java
+++ Hellow World/src/sample/Hellowworld.java
@@ -3,7 +3,7 @@ package sample;
 public class Hellowworld {
 	public static void main(String[] arge){
 		System.out.print("こんにちは世界");
-		System.out.print("さよなら世界");
+		System.out.print("さようなら世界");
 
 	}
 
