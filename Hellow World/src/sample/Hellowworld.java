b22156a43d615986fbbbccc1b39fd9328e0cf716
Yuuta Mikami <ougiakg@gmail.com>
2018-07-05 15:50:20 +0900

    あけましておめでとうございます

--- Hellow World/src/sample/Hellowworld.java
+++ Hellow World/src/sample/Hellowworld.java
@@ -4,7 +4,7 @@ public class Hellowworld {
 	public static void main(String[] arge){
 		System.out.print("こんにちは世界");
 		System.out.print("さようなら世界");
-
+        System.out.println("あけましておめでとうございます");
 	}
 
 }
